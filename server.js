21c61aff1ead4e3cf5d98fc3685cfea37d0be6fa
justin tang <justintang@justins-mbp.sg.ibm.com>
2019-12-19 15:39:31 +0800

    add temp logging middleware morgan

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // load env vars
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
+
+//middleware for logging dev
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
 
 //routing files
 const bootcampsRoutes = require('./routes/bootcamps');
