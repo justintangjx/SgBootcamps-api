5e828afaf6b55fa9b1e37c95fa2f7c85b2acac7d
justin tang <justintang@justins-mbp.sg.ibm.com>
2019-12-20 12:08:35 +0800

    setup mongodb and mongoose and connect locally

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env'});
 
+// connect to DB
+connectDB();
+
 const app = express();
 
-//middleware for logging dev
+// middleware for logging dev
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 };
 
-//routing files
+// routing files
 const bootcampsRoutes = require('./routes/bootcamps');
 
-//mount routers
+// mount routers
 app.use('/api/v1/bootcamps', bootcampsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// global handler to handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`ERROR: ${err.message}`);
+    //close server and exit process
+    server.close(() => process.exit(1));
+});
