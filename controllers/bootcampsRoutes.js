20f29b79048d592e0dff93b3fc5206850fb39073
justin tang <justintang@justins-mbp.sg.ibm.com>
2020-01-02 14:18:49 +0800

    added user model for auth

--- controllers/bootcampsRoutes.js
+++ controllers/bootcampsRoutes.js
@@ -35,11 +35,38 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query.sort('-createdAt');
   }
 
+  //pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await BootcampSchema.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  //executing query
   const bootcamps = await query;
   // const bootcamps = await BootcampSchema.find();
+
+  //pagination result
+  const pagination = {};
+
+  if(endIndex < total){
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+  if(startIndex > 0){
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
   res
     .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+    .json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
 });
 
 // get all bootcamps
