831f2788abd88c1c196aef9250d45f49e45c3b65
justin tang <justintang@justins-MacBook-Pro.local>
2020-01-28 12:57:52 +0800

    setup post operation to add single bootcamp

--- controllers/coursesRoutes.js
+++ controllers/coursesRoutes.js
@@ -1,4 +1,5 @@
 const CourseSchema = require("../models/Course");
+const BootcampSchema = require("../models/Bootcamp");
 const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
 
@@ -7,22 +8,67 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route GET /api/v1/bootcamps/:bootcampId/courses
 // @access public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    let query;
-    
-    if(req.params.bootcampId) {
-        query = CourseSchema.find({ bootcamp: req.params.bootcampId });
-    } else {
-        query = CourseSchema.find().populate({
-            path: 'bootcamp',
-            select: 'name description'
-        });
-    }
+  let query;
 
-    const courses = await query;
-
-    res.status(200).json({
-        success: true,
-        count: courses.length,
-        data: courses
+  if (req.params.bootcampId) {
+    query = CourseSchema.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = CourseSchema.find().populate({
+      path: "bootcamp",
+      select: "name description"
     });
-})
+  }
+
+  const courses = await query;
+
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses
+  });
+});
+
+// get single bootcamp
+// @route GET /api/v1/bootcamps/:id
+// @access public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await (await CourseSchema.findById(req.params.id)).populated({
+    path: "bootcamp",
+    select: "name description"
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`no course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
+
+// add course , after login
+// @route POST /api/v1/bootcamps/:bootcampId/courses
+// @access private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await BootcampSchema.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`no bootcamp with the id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  const course = await CourseSchema.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
