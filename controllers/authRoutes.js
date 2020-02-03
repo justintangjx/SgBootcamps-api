9427f73d1991a51a3c54fca42622338e8459040b
justin tang <just.think.tang@gmail.com>
2020-02-03 16:30:01 +0800

    fix mispelling

--- controllers/authRoutes.js
+++ controllers/authRoutes.js
@@ -76,7 +76,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 // @ desc   Logout User
 // @route   GET /api/v1/auth/logout
 // @access  Private
-exports.logout = ayncHandler(async (req, res, next) => {
+exports.logout = asyncHandler(async (req, res, next) => {
   res.cookie("token", "none", {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true
