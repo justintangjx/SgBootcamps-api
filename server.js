99779d5f4a115bb2ddb242f293c101fd6700bf5c
justin tang <just.think.tang@gmail.com>
2020-02-03 15:55:00 +0800

    use pre-defined middleware for cors, rate limit and cross-scripting attacks

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
-const cookieParser = require('cookie-parser');
-const errorHandler = require('./middleware/error');
-const mongoSanitize = require('express-mongo-sanitize');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+// security
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const cors = require('cors');
 
 // load env vars
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 // connect to DB
 connectDB();
@@ -17,34 +22,51 @@ app.use(express.json());
 app.use(cookieParser());
 
 // middleware for logging dev
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-};
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // sanitize login data to prevent no-sql injection with operator
 app.use(mongoSanitize());
+// set security headers
+app.use(helmet());
+// prevent cross site scripting attacks
+app.use(xss());
+// limit number of requests
+const limiter = rateLimit({
+    windowMs: 10*60*1000, //10mins
+    max: 100 //100 requests per 10 minutes
+});
+app.use(limiter);
+// enable CORS
+app.use(cors());
 
 // routing files
-const bootcampsRoutes = require('./routes/bootcamps');
-const coursesRoutes = require('./routes/courses');
-const authRoutes = require('./routes/auth');
-const reviewsRoutes = require('./routes/reviews');
+const bootcampsRoutes = require("./routes/bootcamps");
+const coursesRoutes = require("./routes/courses");
+const authRoutes = require("./routes/auth");
+const reviewsRoutes = require("./routes/reviews");
 
 // mount routers
-app.use('/api/v1/bootcamps', bootcampsRoutes);
-app.use('/api/v1/courses', coursesRoutes);
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/reviews', reviewsRoutes);
+app.use("/api/v1/bootcamps", bootcampsRoutes);
+app.use("/api/v1/courses", coursesRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/reviews", reviewsRoutes);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`server is running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(
+    `server is running on ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
 
 // global handler to handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`ERRORSERVER: ${err.message}`);
-    //close server and exit process
-    server.close(() => process.exit(1));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`ERRORSERVER: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
 });
